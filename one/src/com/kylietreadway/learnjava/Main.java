6fdb58b3cba72df61187255ac2c78925d4a09efc
Shaun Coyne <mickcoyne1999@gmail.com>
2020-05-07 17:37:36 -0500

    Added hello world example!

--- one/src/com/kylietreadway/learnjava/Main.java
+++ one/src/com/kylietreadway/learnjava/Main.java
@@ -1,5 +1,24 @@
-package com.kylietreadway.learnjava;
+package com.kylietreadway.learnjava; //this is our package
 
+
+//This is our main class. Everything in Java is an object, even our program!
 public class Main {
-    //Hello Kylie
+
+    //this is a comment. This does not get compiled into the .class file
+
+
+    //Main method. This is the entry point to the program. This is static because we can only have 1 of our program.
+    //Think how we can't make a copy of our universe....
+    public static void main(String args[]){
+        //This method is public, because it can be accessed by things outside our object/class :)
+        //this method is "void" because it does not return anything. The user starts the program and that is it.
+        //main is just the name of the method, we always need a main as an entry point to the program. This method
+        //is called automatically.
+
+        //Hello world example :)
+        System.out.println("Hello World!"); // We are using the System.out package. We are using the  println method,
+                                            // we pass a String object
+
+    }
+
 }
