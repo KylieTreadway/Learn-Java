8f392c241d0da6143958da018e3636f179773142
kylietreadway <oldtimeportato@gmail.com>
2020-05-13 02:33:28 -0400

    Created new class

---
+++ one/src/com/kylietreadway/learnjava/Zodiac.java
@@ -0,0 +1,12 @@
+package com.kylietreadway.learnjava;
+
+public class Zodiac {
+
+    private int rudenessLevel;
+    private boolean hasEmpathy;
+
+    public Zodiac(int rudenessLevel, boolean hasEmpathy) {
+        this.rudenessLevel = rudenessLevel;
+        this.hasEmpathy = hasEmpathy;
+    }
+}
