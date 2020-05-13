82908c5ceae5879cca96774e3f6a25c694ae06d0
kylietreadway <oldtimeportato@gmail.com>
2020-05-13 02:32:03 -0400

    Created new class

--- one/src/com/kylietreadway/learnjava/Main.java
+++ one/src/com/kylietreadway/learnjava/Main.java
@@ -1,8 +1,20 @@
 package com.kylietreadway.learnjava;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("Hello World");
 
+
+        Zodiac scorpio;
+
+        scorpio = new Zodiac(10, false);
+
+        Zodiac pisces = new Zodiac(8, true);
+
     }
-}
+
+
+
+
+    }
+
