dc5f02e48d839adfa83dcb0ce1c329bbbad9aecc
kylietreadway <oldtimeportato@gmail.com>
2020-05-13 20:53:11 -0400

    Created ArrayList

--- one/src/com/kylietreadway/learnjava/Main.java
+++ one/src/com/kylietreadway/learnjava/Main.java
@@ -1,5 +1,7 @@
 package com.kylietreadway.learnjava;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello World");
@@ -11,10 +13,19 @@ public class Main {
 
         Zodiac pisces = new Zodiac(8, true);
 
+        scorpio.introduction();
+        pisces.introduction();
+
+        ArrayList<Zodiac> myList = new ArrayList<Zodiac>();
+
+        myList.add(scorpio);
+        myList.add(pisces);
+
+        for (Zodiac zodiacs : myList) {
+            zodiacs.greet();
+            System.out.println("This zodiacs rudeness level is:" + zodiacs.getRudenessLevel());
+
+        }
+
     }
-
-
-
-
-    }
-
+}
--- one/src/com/kylietreadway/learnjava/Zodiac.java
+++ one/src/com/kylietreadway/learnjava/Zodiac.java
@@ -9,4 +9,25 @@ public class Zodiac {
         this.rudenessLevel = rudenessLevel;
         this.hasEmpathy = hasEmpathy;
     }
-}
+
+    public void greet() {
+        System.out.println("Hello");
+    }
+
+    public void introduction() {
+        if (hasEmpathy) {
+            System.out.println("How are you?");
+        }
+
+    }
+
+    public int getRudenessLevel() {
+        return rudenessLevel;
+    }
+     public boolean hasEmpathy() {
+         return hasEmpathy;
+    }
+     public void setHasEmpathy(boolean hasEmpathy) {
+         this.hasEmpathy = hasEmpathy;
+    }
+    }
